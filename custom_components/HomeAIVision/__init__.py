0414eabb29a63af638b8d48ec9437ff29325f040
mwalas <matwal777@gmail.com>
2024-05-23 01:53:57 +0200

    remove api import

--- custom_components/HomeAIVision/__init__.py
+++ custom_components/HomeAIVision/__init__.py
@@ -6,7 +6,6 @@ from homeassistant.const import EVENT_HOMEASSISTANT_START
 from .const import DOMAIN
 from .camera import setup_periodic_camera_check
 from .mod_view import async_setup_view
-from .api import async_register_http_endpoints
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     async def start_camera_analysis(event):
@@ -15,6 +14,5 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, start_camera_analysis)
 
     await async_setup_view(hass)
-    await async_register_http_endpoints(hass)
 
     return True
